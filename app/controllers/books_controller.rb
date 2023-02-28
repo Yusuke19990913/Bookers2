40d38e7ce2af8d7ee949323a2b2333ccd6f984a6
Yusuke19990913 <73863256+Yusuke19990913@users.noreply.github.com>
2023-02-28 12:14:17 +0000

    完成１

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+    before_action :is_matching_login_user, only: [:edit, :update]
+  def new
+    @book=Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice]="You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render "books/index"
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]= "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render "books/edit"
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book=Book.new
+  end
+
+  def show
+    @book=Book.find(params[:id])
+    @bookNew =Book.new
+    @books = Book.all
+  end
+
+  def destroy
+   book =Book.find(params[:id])
+   book.destroy
+   redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book=Book.find(params[:id])
+    user_id =book.user.id
+    unless user_id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/about' => "homes#about" ,as: "about"
+  get 'home/top' => "homes#top"
+  devise_for :users
+  root to:'homes#top'
+  resources :books, only: [:new, :index, :show,:create,:destroy,:edit,:update]
+  resources :users,only:[:show, :edit,:index,:update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
